8ae0ec681ec3799d5e8f7747b273f5860bc5b0b8
yorvinlunawisky <yorvin0506@gmail.com>
2020-05-23 15:52:10 -0400

    Transition menu added

--- mobile-react-app/src/Components/Navigation.js
+++ mobile-react-app/src/Components/Navigation.js
@@ -1,24 +1,22 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { useTransition, animated } from 'react-spring';
 
 const Navigation = () => {
   const [showMenu, setShowMenu] = useState(false);
 
-  let menu
-  let menuMask
-  if (showMenu) {
-    menu =
-      <div className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow">
-        The menu
-      </div>
+  const maskTranssition = useTransition(showMenu, null, {
+    from: { position: 'absolute', opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
 
-    menuMask =
-      <div className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
-        onClick={() => setShowMenu(false)}>
-
-      </div>
-  }
+  const menuTransition = useTransition(showMenu, null, {
+    from: { opacity: 0, transform: 'translateX(-100%)' },
+    enter: { opacity: 1, transform: 'translateX(0%' },
+    leave: { opacity: 0, transform: 'translateX(-100%' },
+  });
 
 
   return (
@@ -28,8 +26,35 @@ const Navigation = () => {
           icon={faBars}
           onClick={() => setShowMenu(!showMenu)} />
       </span>
-      {menuMask}
-      {menu}
+
+      {
+        maskTranssition.map(({ item, key, props }) =>
+          item &&
+          <animated.div
+            key={key}
+            style={props}
+            className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
+            onClick={() => setShowMenu(false)}>
+          </animated.div>
+        )
+      }
+
+      {
+        menuTransition.map(({ item, key, props }) =>
+          item &&
+          <animated.div
+            key={key}
+            style={props}
+            className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow p-3">
+            <span className="font-bold">
+              This is the menu
+              </span>
+            <ul>
+              <li>Home</li>
+            </ul>
+          </animated.div>
+        )
+      }
     </nav>
   );
 }
